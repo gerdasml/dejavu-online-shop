3652031a8032a11a9203bcdf56c8ff8f4256770e
STB\vyst01 <vytautas.strimaitis@spp.se>
2018-02-26 15:48:22 +0200

    Changed line ending to Windows

--- src/main/frontend/webpack.config.js
+++ src/main/frontend/webpack.config.js
@@ -1,10 +1,10 @@
-var path = require('path');
-const merge = require('webpack-merge');
+var path = require("path");
+const merge = require("webpack-merge");
 
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
-    source: path.join(__dirname, 'app'),
-    output: path.join(__dirname, '../../../target/classes/static')
+    source: path.join(__dirname, "app"),
+    output: path.join(__dirname, "../../../target/classes/static")
 };
 
 const common = {
@@ -15,41 +15,41 @@ const common = {
     debug: true,
     output: {
         path: PATHS.output,
-        publicPath: '',
-        filename: 'bundle.js'
+        publicPath: "",
+        filename: "bundle.js"
     },
     module: {
         loaders: [{
             exclude: /node_modules/,
-            loader: 'babel'
+            loader: "babel"
         }, {
             test: /\.css$/,
-            loader: 'style!css'
+            loader: "style!css"
         }]
     },
     resolve: {
-        extensions: ['', '.js', '.jsx']
+        extensions: ["", ".js", ".jsx"]
     }
 };
 
-if (TARGET === 'start' || !TARGET) {
+if (TARGET === "start" || !TARGET) {
     module.exports = merge(common, {
         devServer: {
             port: 9090,
             proxy: {
-                '/': {
-                    target: 'http://localhost:8080',
+                "/": {
+                    target: "http://localhost:8080",
                     secure: false,
                     prependPath: false
                 }
             },
-            publicPath: 'http://localhost:9090/',
+            publicPath: "http://localhost:9090/",
             historyApiFallback: true
         },
-        devtool: 'source-map'
+        devtool: "source-map"
     });
 }
 
-if (TARGET === 'build') {
+if (TARGET === "build") {
     module.exports = merge(common, {});
 }
