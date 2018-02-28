4cfced36ea764e963d3eb4ad0335ab8be96470ad
vstrimaitis <vstrimaitis@gmail.com>
2018-02-28 12:51:29 +0200

    Changed .jsx files to .tsx files, switched eslint for tslint, updated dependencies.

--- src/main/frontend/app/index.jsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from "react-dom";
-import App from "./App";
-import "../style/style.css";
-
-ReactDOM.render(<App />, document.getElementById("react"));
--- src/main/frontend/webpack.config.js
+++ src/main/frontend/webpack.config.js
@@ -28,10 +28,23 @@ const common = {
                 test: /\.css$/,
                 loader: "style!css"
             }
+        ],
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
         ]
     },
     resolve: {
-        extensions: ["", ".js", ".jsx"]
+        extensions: ["", ".js", ".jsx", ".ts", ".tsx"]
     }
 };
 
