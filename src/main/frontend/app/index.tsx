53a64cf09d485323b01eca77c208413d515f9d4f
vstrimaitis <vstrimaitis@gmail.com>
2018-02-28 13:20:30 +0200

    Fixed wrong mount element name, added the example style.

--- src/main/frontend/app/index.tsx
+++ src/main/frontend/app/index.tsx
@@ -2,8 +2,9 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { Hello } from "./Hello";
+import "../style/style.css";
 
 ReactDOM.render(
     <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+    document.getElementById("react")
 );
