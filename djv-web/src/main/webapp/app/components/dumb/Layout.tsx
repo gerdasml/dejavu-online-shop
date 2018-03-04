356d44958df908680ffa8180c0c63b583ad848b1
Gerda Šimoliūnaitė <sml.gerda@gmail.com>
2018-03-04 18:00:36 +0200

    Added header in main page, also added style to it

---
+++ djv-web/src/main/webapp/app/components/dumb/Layout.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+import { Drawer } from './Drawer';
+import { Header } from './Header';
+
+export const Layout = () => 
+    <div>
+        <Header />
+        <Drawer />
+    </div>
