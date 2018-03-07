430996053298f41127beaa4b3d73a760b49a2414
vstrimaitis <vstrimaitis@gmail.com>
2018-03-07 23:11:28 +0200

    Added password hashing.

--- djv-auth/src/main/java/lt/dejavu/auth/service/AuthServiceImpl.java
+++ djv-auth/src/main/java/lt/dejavu/auth/service/AuthServiceImpl.java
@@ -1,6 +1,7 @@
 package lt.dejavu.auth.service;
 
 import lt.dejavu.auth.exception.UserAlreadyExistsException;
+import lt.dejavu.auth.helpers.Hasher;
 import lt.dejavu.auth.model.User;
 import lt.dejavu.auth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,24 +13,29 @@ import java.util.UUID;
 @Service
 public class AuthServiceImpl implements AuthService {
     private final UserRepository userRepository;
+    private final Hasher hasher;
 
     @Autowired
-    public AuthServiceImpl(UserRepository userRepository) {
+    public AuthServiceImpl(UserRepository userRepository, Hasher hasher) {
         this.userRepository = userRepository;
+        this.hasher = hasher;
     }
 
     @Override
     public void register(User info) {
-        int id = userRepository.getUserId(info.getEmail(), info.getPassword());
+        String passHash = hasher.hash(info.getPassword());
+        int id = userRepository.getUserId(info.getEmail(), passHash);
         if (id != 0) {
             throw new UserAlreadyExistsException("A user with the specified credentials already exists");
         }
+        info.setPassword(passHash);
         userRepository.addUser(info);
     }
 
     @Override
     public UUID login(String email, String pass) {
-        int userId = userRepository.getUserId(email, pass);
+        String passHash = hasher.hash(pass);
+        int userId = userRepository.getUserId(email, passHash);
         if (userId == 0) {
             // User not found
             // TODO: throw 404
