75ebb5d780f21da34c035e1b015f328b07fbeecf
vstrimaitis <vstrimaitis@gmail.com>
2018-03-07 11:51:53 +0200

    Extracted table and column names into properties file.

--- djv-auth/src/main/java/lt/dejavu/auth/db/dao/UserDAO.java
+++ djv-auth/src/main/java/lt/dejavu/auth/db/dao/UserDAO.java
@@ -5,13 +5,15 @@ import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 
 public interface UserDAO {
-    String TABLE_NAME = "User";
-    String ID = "Id";
-    String EMAIL = "Email";
-    String PASSWORD = "Password";
-    String FIRST_NAME = "FirstName";
-    String LAST_NAME = "LastName";
-    String TYPE = "Type";
+    // These should match the in ${flyway.placeholders.tables.user}
+    // TODO: investigate if the constants can be connected to the properties
+    String TABLE_NAME = "user";
+    String ID = "id";
+    String EMAIL = "email";
+    String PASSWORD = "password";
+    String FIRST_NAME = "firstName";
+    String LAST_NAME = "lastName";
+    String TYPE = "userType";
 
     @SqlQuery("SELECT " + ID + ", " + EMAIL + ", " + PASSWORD + ", " + FIRST_NAME + ", " + LAST_NAME + ", " + TYPE + " " +
               "FROM " + TABLE_NAME + " " +
--- djv-auth/src/main/resources/db/migration/V1__create_user_and_auth_tables.sql
+++ djv-auth/src/main/resources/db/migration/V1__create_user_and_auth_tables.sql
@@ -1,15 +1,15 @@
-CREATE TABLE user (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  firstName VARCHAR(255),
-  lastName VARCHAR(255),
-  userType VARCHAR(10) NOT NULL
+CREATE TABLE ${tables.user.name} (
+  ${tables.user.columns.id} INT AUTO_INCREMENT PRIMARY KEY,
+  ${tables.user.columns.email} VARCHAR(255) UNIQUE NOT NULL,
+  ${tables.user.columns.password} VARCHAR(255) NOT NULL,
+  ${tables.user.columns.firstName} VARCHAR(255),
+  ${tables.user.columns.lastName} VARCHAR(255),
+  ${tables.user.columns.type} VARCHAR(10) NOT NULL
 );
 
-CREATE TABLE userToken (
-  userId INT PRIMARY KEY,
-  accessToken UUID UNIQUE,
+CREATE TABLE ${tables.userToken.name} (
+  ${tables.userToken.columns.id} INT PRIMARY KEY,
+  ${tables.userToken.columns.token} UUID UNIQUE,
 
-  FOREIGN KEY (userId) REFERENCES user(id)
+  FOREIGN KEY (${tables.userToken.columns.id}) REFERENCES user(${tables.user.columns.id})
 );
