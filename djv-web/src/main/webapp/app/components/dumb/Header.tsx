b6c41e08b8f812a73fcfd026733d5d3aeffed188
Margiris Strakšys <margirisstr@gmail.com>
2018-03-09 01:33:25 +0200

    header grid with icons made.

--- djv-web/src/main/webapp/app/components/dumb/Header.tsx
+++ djv-web/src/main/webapp/app/components/dumb/Header.tsx
@@ -1,18 +1,72 @@
 import * as React from "react";
 
+import {Button, Grid, Icon, Image, Search} from "semantic-ui-react";
 import "../../../style/header.css";
+// import logo from "../../assets/dejavu-logo-transperant.png";
 
 /*export const Header = () =>
     <div className="header"></div>
 ;*/
 
 export default class Header extends React.Component {
+    /*constructor () {
+        super(props: {});
+        this.state = {
+            test: "test"
+        };
+    }*/
 
-    render(){
+    render () {
         return (
-            <div>
-
-            </div>
-        )
+            <Grid className="header">
+                <Grid.Column width={3}>
+                    <Image id="logo"
+                           // TODO: change for local image when loaders are fixed
+                           src="http://www.part.lt/img/1db15aa8a0de669f04c364eae9b6edb9383.png"
+                           alt="Neveikia"
+                    />
+                </Grid.Column>
+                <Grid.Column width={13}>
+                    <Grid.Row>
+                        <Grid className="header">
+                            <Grid.Column width={6}>
+                                <Icon name="mail outline" />
+                                dejavu.psk@gmail.com
+                            </Grid.Column>
+                            <Grid.Column width={6}>
+                                <Icon name="phone" />
+                                +3706NETIKRAS
+                            </Grid.Column>
+                            <Grid.Column width={4}>
+                                {/* TODO: make as a button */}
+                                <Icon name="info" />
+                                INFO
+                            </Grid.Column>
+                        </Grid>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid className="header">
+                            <Grid.Column width={12}>
+                                {/* TODO: implement search */}
+                                <Search id="searchBar"
+                                        value="Search..."
+                                        noResultsMessage="Not implemented exception. xD"
+                                        size="mini"
+                                >
+                                </Search>
+                            </Grid.Column>
+                            <Grid.Column width={2}>
+                                {/* TODO: make as a button */}
+                                <Icon name ="cart"/>
+                            </Grid.Column>
+                            <Grid.Column width={2}>
+                                {/* TODO: make as a button */}
+                                <Icon name ="user circle outline"/>
+                            </Grid.Column>
+                        </Grid>
+                    </Grid.Row>
+                </Grid.Column>
+            </Grid>
+        );
     }
 }
--- djv-web/src/main/webapp/app/components/dumb/Layout.tsx
+++ djv-web/src/main/webapp/app/components/dumb/Layout.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 import { DrawerMenu } from "./Drawer";
-import { Header } from "./Header";
+import Header from "./Header";
 
 export const Layout = () =>
     <div>
