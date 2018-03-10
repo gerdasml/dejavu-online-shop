38123045b4ea4a32f0f259ef9a33508d97bc501b
vstrimaitis <vstrimaitis@gmail.com>
2018-03-10 18:03:49 +0200

    Disabled some tslint rules, added a sample of how to use local images. Added index.d.ts file to overcome TypeScript's module-import weirdness.

--- djv-web/src/main/webapp/app/components/dumb/Drawer.tsx
+++ djv-web/src/main/webapp/app/components/dumb/Drawer.tsx
@@ -4,6 +4,8 @@ import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui
 
 import "../../../style/drawer.css";
 
+const myImage = require("../../assets/placeholder_350x150.png");
+
 export interface IDrawerMenuState { visible: boolean; }
 
 export class DrawerMenu extends React.Component<{}, IDrawerMenuState> {
@@ -19,7 +21,10 @@ export class DrawerMenu extends React.Component<{}, IDrawerMenuState> {
                 <Sidebar.Pushable as={Segment}>
                 <Sidebar className="drawer" as={Menu} animation='push' width='thin' visible={this.state.visible} icon='labeled' vertical inverted/>
                 <Sidebar.Pusher>
-                    <Segment basic className="content"/>
+                    <Segment basic className="content">
+                        <button onClick={() => this.setState({visible: !this.state.visible})}>go</button>
+                        <Image src={myImage} />
+                    </Segment>
                 </Sidebar.Pusher>
                 </Sidebar.Pushable>
             </div>
