a2a89c53764fa2b435a81f08c692562d0b32e875
Margiris Strakšys <margirisstr@gmail.com>
2018-03-12 14:05:57 +0200

    header styles and grid structure changed.

--- djv-web/src/main/webapp/app/components/dumb/Header.tsx
+++ djv-web/src/main/webapp/app/components/dumb/Header.tsx
@@ -19,53 +19,55 @@ export default class Header extends React.Component {
     render () {
         return (
             <Grid className="header">
-                <Grid.Column width={3}>
-                    <Image id="logo"
-                           // TODO: change for local image when loaders are fixed
-                           src="http://www.part.lt/img/1db15aa8a0de669f04c364eae9b6edb9383.png"
-                           alt="Neveikia"
-                    />
-                </Grid.Column>
-                <Grid.Column width={13}>
-                    <Grid.Row>
-                        <Grid className="header">
-                            <Grid.Column width={6}>
-                                <Icon name="mail outline" />
-                                dejavu.psk@gmail.com
-                            </Grid.Column>
-                            <Grid.Column width={6}>
-                                <Icon name="phone" />
-                                +3706NETIKRAS
-                            </Grid.Column>
-                            <Grid.Column width={4}>
-                                {/* TODO: make as a button */}
-                                <Icon name="info" />
-                                INFO
-                            </Grid.Column>
+                <Grid.Row>
+                    <Grid.Column width={3}>
+                        <Image id="logo"
+                            // TODO: change for local image when loaders are fixed
+                            src="http://www.part.lt/img/1db15aa8a0de669f04c364eae9b6edb9383.png"
+                            alt="Neveikia"
+                        />
+                    </Grid.Column>
+                    <Grid.Column width={13}>
+                        <Grid>
+
+                            <Grid.Row>
+                                <Grid.Column width={6}>
+                                    <Icon name="mail outline" />
+                                    dejavu.psk@gmail.com
+                                </Grid.Column>
+                                <Grid.Column width={6}>
+                                    <Icon name="phone" />
+                                    +3706NETIKRAS
+                                </Grid.Column>
+                                <Grid.Column width={4}>
+                                    {/* TODO: make as a button */}
+                                    <Icon name="info" />
+                                    INFO
+                                </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
+                                <Grid.Column width={12}>
+                                    {/* TODO: implement search */}
+                                    <Search id="searchBar"
+                                            value="Search..."
+                                            noResultsMessage="Not implemented exception. xD"
+                                            size="mini"
+                                            fluid
+                                    >
+                                    </Search>
+                                </Grid.Column>
+                                <Grid.Column width={2}>
+                                    {/* TODO: make as a button */}
+                                    <Icon name ="cart"/>
+                                </Grid.Column>
+                                <Grid.Column width={2}>
+                                    {/* TODO: make as a button */}
+                                    <Icon name ="user circle outline"/>
+                                </Grid.Column>
+                            </Grid.Row>
                         </Grid>
-                    </Grid.Row>
-                    <Grid.Row>
-                        <Grid className="header">
-                            <Grid.Column width={12}>
-                                {/* TODO: implement search */}
-                                <Search id="searchBar"
-                                        value="Search..."
-                                        noResultsMessage="Not implemented exception. xD"
-                                        size="mini"
-                                >
-                                </Search>
-                            </Grid.Column>
-                            <Grid.Column width={2}>
-                                {/* TODO: make as a button */}
-                                <Icon name ="cart"/>
-                            </Grid.Column>
-                            <Grid.Column width={2}>
-                                {/* TODO: make as a button */}
-                                <Icon name ="user circle outline"/>
-                            </Grid.Column>
-                        </Grid>
-                    </Grid.Row>
-                </Grid.Column>
+                    </Grid.Column>
+                </Grid.Row>
             </Grid>
         );
     }
