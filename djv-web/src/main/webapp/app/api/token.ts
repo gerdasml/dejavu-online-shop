03a91ac7e97fb3835fee8ae25e60abb385a247aa
vstrimaitis <vstrimaitis@gmail.com>
2018-03-19 17:03:30 +0200

    Fixes after code review.

--- djv-web/src/main/webapp/app/api/token.ts
+++ djv-web/src/main/webapp/app/api/token.ts
@@ -7,6 +7,6 @@ export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const clearToken = () => localStorage.removeItem(TOKEN_KEY);
 
 export const buildAuthHeader = () => {
-    const tkn = getToken();
-    return "Bearer " + tkn;
+    const token = getToken();
+    return "Bearer " + token;
 };
