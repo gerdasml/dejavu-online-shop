b90f3bedb1b8578d3f69859a02b6804503633b52
vstrimaitis <vstrimaitis@gmail.com>
2018-04-02 20:56:54 +0300

    Removed unnecessary comments.

--- djv-web/src/main/webapp/app/components/views/Home.tsx
+++ djv-web/src/main/webapp/app/components/views/Home.tsx
@@ -4,20 +4,9 @@ import { Header, Image, Card } from "semantic-ui-react";
 import {products} from "../../data/products";
 import { ProductCard } from "../dumb/Home/ProductCard";
 
-const myImage = require("../../assets/placeholder_350x150.png");
-
 export const Home = () => (
     <div>
         <Header size="large">Home</Header>
-        {/* <Image src={myImage} />
-        <Image src={myImage} />
-        <Image src={myImage} />
-        <Image src={myImage} />
-        <Image src={myImage} />
-        <Image src={myImage} />
-        <Image src={myImage} />
-        <Image src={myImage} />
-        <Image src={myImage} /> */}
         <Card.Group itemsPerRow={5} doubling>
             {products.map((x, i) => <ProductCard key={i} product={x} />)}
         </Card.Group>
