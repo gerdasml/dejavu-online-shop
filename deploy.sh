0f994da213d56c76ef8d7a94e864112d89271c2c
Server <vstrimaitis@gmail.com>
2018-04-08 12:02:52 +0000

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Terminate script if anything fails
+set -e
+# Build the project
+mvn clean install
+
+# Get ID of running java procesees (our app)
+pid=$(pgrep java || echo "")
+# If any processes were found, kill them
+if [ -n "$pid" ]; then kill -9 $pid; fi
+
+# Run the web app
+cd djv-web
+mvn spring-boot:run &
+cd ..
