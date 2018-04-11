aef0f66209aff32d20a2a62390323adb7cdef43f
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-04-11 18:29:05 +0300

    Added example file upload functionality, added method to resolve ImageFormat

--- djv-storage/src/main/java/lt/dejavu/storage/ImageApi.java
+++ djv-storage/src/main/java/lt/dejavu/storage/ImageApi.java
@@ -1,10 +1,41 @@
 package lt.dejavu.storage;
 
+import lt.dejavu.storage.model.ImageFormat;
+import lt.dejavu.storage.strategy.StorageStrategy;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("${rest.basePath}/image")
 public class ImageApi {
+    @Autowired
+    private StorageStrategy storageStrategy;
 
+    @Autowired
+    private List<ImageFormat> allowedFormats;
+
+
+    @PostMapping("/upload")
+    public void uploadImage(@RequestParam("file") MultipartFile file) throws IOException {
+        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+        ImageFormat format = ImageFormat.resolve(extension);
+        if(format == ImageFormat.UNKNOWN) {
+            // TODO: proper exception
+            throw new RuntimeException("Unknown file format");
+        }
+        if(!allowedFormats.contains(format)) {
+            // TODO: proper exception
+            throw new RuntimeException("Unsupported file format");
+        }
+        byte[] content = file.getBytes();
+        storageStrategy.saveFile(content, 10, format);
+    }
 }
--- djv-storage/src/main/java/lt/dejavu/storage/model/ImageFormat.java
+++ djv-storage/src/main/java/lt/dejavu/storage/model/ImageFormat.java
@@ -6,7 +6,8 @@ import org.springframework.http.MediaType;
 @Getter
 public enum ImageFormat {
     PNG("png", MediaType.IMAGE_PNG_VALUE),
-    JPG("jpg", MediaType.IMAGE_JPEG_VALUE);
+    JPG("jpg", MediaType.IMAGE_JPEG_VALUE),
+    UNKNOWN("", "");
 
     private String extension;
     private String mediaType;
@@ -14,5 +15,13 @@ public enum ImageFormat {
     ImageFormat(String extension, String mediaType) {
         this.extension = extension;
         this.mediaType = mediaType;
+    }
+
+    public static ImageFormat resolve(String s) {
+        try {
+            return ImageFormat.valueOf(s);
+        } catch(IllegalArgumentException e) {
+            return UNKNOWN;
+        }
     }
 }
