de2834822ff3df6bb23fe79aa7db161c50c30fa1
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-04-11 15:38:45 +0300

    DB data type compatibility issues

--- djv-product/src/main/resources/db/migration/V2.1__create_product_table.sql
+++ djv-product/src/main/resources/db/migration/V2.1__create_product_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE ${tables.product.name} (
   ${tables.product.columns.id} BIGINT AUTO_INCREMENT PRIMARY KEY,
   ${tables.product.columns.name} VARCHAR(255) NOT NULL,
-  ${tables.product.columns.description} VARCHAR(80000),
+  ${tables.product.columns.description} ${types.text},
   ${tables.product.columns.creationDate} TIMESTAMP,
   ${tables.product.columns.category} BIGINT,
   ${tables.product.columns.price} DECIMAL
