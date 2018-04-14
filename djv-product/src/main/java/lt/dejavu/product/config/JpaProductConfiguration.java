8d6105ca6aebf22e19b7d290e5e7e42abb72906c
Mantas Petrikas <man.petrikas@gmail.com>
2018-04-14 11:46:54 +0300

    added show sql property

--- djv-product/src/main/java/lt/dejavu/product/config/JpaProductConfiguration.java
+++ djv-product/src/main/java/lt/dejavu/product/config/JpaProductConfiguration.java
@@ -1,9 +1,9 @@
 package lt.dejavu.product.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -12,6 +12,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
@@ -21,19 +22,26 @@ public class JpaProductConfiguration {
     private DataSource dataSource;
 
     @Autowired
-    @Qualifier("databaseDialect")
-    private String databaseDialect;
+    private Environment env;
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setDatabasePlatform(databaseDialect);
+        vendorAdapter.setDatabasePlatform(env.getProperty("spring.datasource.hibernate-dialect"));
         vendorAdapter.setGenerateDdl(false);
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
         em.setDataSource(dataSource);
         em.setPackagesToScan("lt.dejavu");
         em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(hibernateProperties());
         return em;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.show_sql", env.getProperty("spring.datasource.show-sql"));
+        properties.put("hibernate.format_sql", env.getProperty("spring.datasource.show-sql"));
+        return properties;
     }
 
     @Bean
