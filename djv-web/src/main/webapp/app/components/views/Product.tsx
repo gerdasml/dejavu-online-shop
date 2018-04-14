4acbccad8bc00eb090f4730b7b632928381b398e
Gerda Šimoliūnaitė <sml.gerda@gmail.com>
2018-04-14 13:38:38 +0300

    Fixed carousel styling

--- djv-web/src/main/webapp/app/components/views/Product.tsx
+++ djv-web/src/main/webapp/app/components/views/Product.tsx
@@ -5,9 +5,11 @@ import {Header} from "semantic-ui-react";
 import {RouteComponentProps} from "react-router-dom";
 import { Carousel } from "../dumb/Product/Carousel";
 
+import "../../../style/product.css";
+
 interface IProductRouteProps { id: number; }
 export const Product = (props: RouteComponentProps<IProductRouteProps>) => (
-    <div>
+    <div className="productStyle">
         <Header size="large">Product {props.match.params.id}</Header>
         <Carousel />
     </div>
