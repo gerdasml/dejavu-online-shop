e15b4552bc143d8ffe6b5b811e08e4d955145e8f
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-04-14 19:48:08 +0300

    Added log4j2 logging.

--- djv-web/src/main/java/lt/dejavu/web/ShopApplication.java
+++ djv-web/src/main/java/lt/dejavu/web/ShopApplication.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({
-	"lt.dejavu"
+        "lt.dejavu"
 })
 public class ShopApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ShopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApplication.class, args);
+    }
 }
