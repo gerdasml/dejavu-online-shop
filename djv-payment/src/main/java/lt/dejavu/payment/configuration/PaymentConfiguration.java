8f5b6561213884ace87bba705475633c0254eace
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-04-15 19:37:16 +0300

    Fixed ID type, enabled double reading of response body

--- djv-payment/src/main/java/lt/dejavu/payment/configuration/PaymentConfiguration.java
+++ djv-payment/src/main/java/lt/dejavu/payment/configuration/PaymentConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,7 +21,7 @@ public class PaymentConfiguration {
                         paymentProperties.getCredentials().getPassword())
                 .rootUri(paymentProperties.getPaymentServiceAddress())
                 .interceptors(new LoggingRequestInterceptor())
-                .requestFactory(new HttpComponentsClientHttpRequestFactory())
+                .requestFactory(new BufferingClientHttpRequestFactory(new HttpComponentsClientHttpRequestFactory()))
                 .build();
     }
 }
--- djv-payment/src/main/java/lt/dejavu/payment/dto/PaymentDTO.java
+++ djv-payment/src/main/java/lt/dejavu/payment/dto/PaymentDTO.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class PaymentDTO {
-    private Integer id;
+    private String id;
 
     @JsonProperty("created_at")
     private LocalDateTime createdAt;
