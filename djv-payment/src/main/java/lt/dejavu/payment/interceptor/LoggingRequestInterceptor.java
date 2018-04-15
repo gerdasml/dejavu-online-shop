e745ff3b41a459c35d40e38b4141c61c50616630
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-04-15 18:08:21 +0300

    Changed logging graphics

--- djv-payment/src/main/java/lt/dejavu/payment/interceptor/LoggingRequestInterceptor.java
+++ djv-payment/src/main/java/lt/dejavu/payment/interceptor/LoggingRequestInterceptor.java
@@ -28,12 +28,12 @@ public class LoggingRequestInterceptor implements ClientHttpRequestInterceptor {
     }
 
     private void traceRequest(HttpRequest request, byte[] body) {
-        log.debug("---------------------------begin request {}---------------------------------------------", getRequestDescription(request));
+        log.debug("--------------------------- begin request {} ---------------------------------------------", getRequestDescription(request));
         log.debug("Headers     :");
         request.getHeaders().forEach((key, value) -> log.debug("    [{}] {}", key, value));
         log.debug("Request body:");
         Stream.of(new String(body).split("\n")).forEach(log::debug);
-        log.debug("--------------------------end request {}------------------------------------------------", getRequestDescription(request));
+        log.debug("-------------------------- end request {} ------------------------------------------------", getRequestDescription(request));
     }
 
     private void traceResponse(ClientHttpResponse response) throws IOException {
@@ -45,14 +45,14 @@ public class LoggingRequestInterceptor implements ClientHttpRequestInterceptor {
             inputStringBuilder.append('\n');
             line = bufferedReader.readLine();
         }
-        log.debug("............................begin response..........................................");
+        log.debug("~~~~~~~~~~~~~~~~~~~~~~~~~~~~ begin response ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         log.debug("Status code  : {}", response.getStatusCode());
         log.debug("Status text  : {}", response.getStatusText());
         log.debug("Headers      :");
         response.getHeaders().forEach((key, value) -> log.debug("    [{}] {}", key, value));
         log.debug("Response body:");
         Stream.of(inputStringBuilder.toString().split("\n")).forEach(log::debug);
-        log.debug(".............................end response...........................................");
+        log.debug("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ end response ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
 
     private String getRequestDescription(HttpRequest request) {
