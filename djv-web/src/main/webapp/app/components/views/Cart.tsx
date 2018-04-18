23f383821f2d1083cf728e3f5eee0a27d8562d6b
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-04-18 17:50:44 +0300

    Adding steps to view

---
+++ djv-web/src/main/webapp/app/components/views/Cart.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+
+import { Button, Icon, Table } from "semantic-ui-react";
+
+import { IPurchase } from "../../model/Purchase";
+
+import "../../../style/cart.css";
+import { ShoppingCart } from "../smart/Cart/ShoppingCart";
+
+import * as Step from "../dumb/Cart/Step";
+
+import { CartStep, CartStepHeader } from "../dumb/Cart/CartStepHeader";
+
+const purchases: IPurchase[] = [{
+  amount: 2,
+  item: {name: "something", imageUrl:"", description:"", price: 5},
+  total: 100,
+  unitPrice: 50
+}];
+
+interface CartState {
+    currentStep: CartStep;
+}
+
+export class Cart extends React.Component<{}, CartState> {
+    constructor (props: {}) {
+        super(props);
+        this.state = {
+            currentStep: CartStep.CART
+        };
+    }
+
+    nextStep = () => {
+        const step = this.state.currentStep;
+        if(step === CartStep.PAYMENT) {
+            console.log("Finished");
+            // TODO: finish this somehow
+            return;
+        }
+        this.setState({currentStep: this.state.currentStep+1});
+    }
+
+    render () {
+        return (
+            <div>
+                <CartStepHeader active={this.state.currentStep} />
+                {this.state.currentStep === CartStep.CART
+                ? <Step.Cart purchases={purchases} onComplete={this.nextStep} />
+                : ""
+                }
+                {this.state.currentStep === CartStep.DELIVERY_INFO
+                ? <Step.DeliveryInfo onComplete={this.nextStep} />
+                : ""
+                }
+            </div>
+        );
+    }
+}
