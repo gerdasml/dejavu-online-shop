91aa3f9c60bca4816f7fe6bbb4b3cf828541439c
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-04-18 19:25:36 +0300

    Show later steps as disabled

--- djv-web/src/main/webapp/app/components/dumb/Cart/CartStepHeader.tsx
+++ djv-web/src/main/webapp/app/components/dumb/Cart/CartStepHeader.tsx
@@ -18,35 +18,50 @@ export const CartStepHeader = (props: CartStepProps) =>
     (
         <Step.Group size="mini" id="stepGroup">
 
-            <Step active={props.active === CartStep.CART}>
+            <Step
+                active={props.active === CartStep.CART}
+                disabled={props.active < CartStep.CART}
+            >
                 <Icon name="cart"/>
                 <Step.Content>
                     <Step.Title>Cart</Step.Title>
                 </Step.Content>
             </Step>
 
-            <Step active={props.active === CartStep.DELIVERY_INFO}>
+            <Step
+                active={props.active === CartStep.DELIVERY_INFO}
+                disabled={props.active < CartStep.DELIVERY_INFO}
+            >
                 <Icon name="truck"/>
                 <Step.Content>
                     <Step.Title>Delivery info</Step.Title>
                 </Step.Content>
             </Step>
 
-            <Step active={props.active === CartStep.PAYMENT}>
+            <Step
+                active={props.active === CartStep.PAYMENT}
+                disabled={props.active < CartStep.PAYMENT}
+            >
                 <Icon name="payment" />
                 <Step.Content>
                     <Step.Title>Payment</Step.Title>
                 </Step.Content>
             </Step>
 
-            <Step active={props.active === CartStep.CONFIRMATION}>
+            <Step
+                active={props.active === CartStep.CONFIRMATION}
+                disabled={props.active < CartStep.CONFIRMATION}
+            >
                 <Icon name="info" />
                 <Step.Content>
                     <Step.Title>Confirmation</Step.Title>
                 </Step.Content>
             </Step>
 
-            <Step active={props.active === CartStep.APPROVAL}>
+            <Step
+                active={props.active === CartStep.APPROVAL}
+                disabled={props.active < CartStep.APPROVAL}
+            >
                 <Icon name="check" />
                 <Step.Content>
                     <Step.Title>Approvement</Step.Title>
