fc0cb7416f900ff3e81f6c353370b8d10931716f
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-04-18 17:11:47 +0300

    Added ShoppingCart component

---
+++ djv-web/src/main/webapp/app/components/dumb/Cart/CartStepHeader.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+
+import { Icon, Step } from "semantic-ui-react";
+
+import "../../../../style/cart.css";
+
+interface CartStepProps { active: CartStep; }
+
+export enum CartStep {
+    CART,
+    DELIVERY_INFO,
+    PAYMENT,
+    CONFIRMATION,
+    APPROVAL
+}
+
+export const CartStepHeader = (props: CartStepProps) =>
+    (
+        <Step.Group size="mini" id="stepGroup">
+
+            <Step active={props.active === CartStep.CART}>
+                <Icon name="cart"/>
+                <Step.Content>
+                    <Step.Title>Cart</Step.Title>
+                </Step.Content>
+            </Step>
+
+            <Step active={props.active === CartStep.DELIVERY_INFO}>
+                <Icon name="truck"/>
+                <Step.Content>
+                    <Step.Title>Delivery info</Step.Title>
+                </Step.Content>
+            </Step>
+
+            <Step active={props.active === CartStep.PAYMENT}>
+                <Icon name="payment" />
+                <Step.Content>
+                    <Step.Title>Payment</Step.Title>
+                </Step.Content>
+            </Step>
+
+            <Step active={props.active === CartStep.CONFIRMATION}>
+                <Icon name="info" />
+                <Step.Content>
+                    <Step.Title>Confirmation</Step.Title>
+                </Step.Content>
+            </Step>
+
+            <Step active={props.active === CartStep.APPROVAL}>
+                <Icon name="check" />
+                <Step.Content>
+                    <Step.Title>Approvement</Step.Title>
+                </Step.Content>
+            </Step>
+        </Step.Group>
+    );
--- djv-web/src/main/webapp/app/components/views/Cart.tsx
+++ djv-web/src/main/webapp/app/components/views/Cart.tsx
@@ -6,9 +6,10 @@ import { IPurchase } from "../../model/Purchase";
 
 import { Amount } from "../dumb/Cart/Amount";
 
-import { CartStep } from "../dumb/Cart/CartStep";
+import { CartStepHeader } from "../dumb/Cart/CartStepHeader";
 
 import "../../../style/cart.css";
+import { ShoppingCart } from "../smart/Cart/ShoppingCart";
 
 interface ICartProps { purchases: IPurchase[]; }
 
@@ -20,33 +21,34 @@ const purchases: IPurchase[] = [{
 }];
 
 export const Cart = (props: {}) => (
-  <div>
-    <CartStep />
-    <Table striped celled>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>Item</Table.HeaderCell>
-          <Table.HeaderCell>Unit Price</Table.HeaderCell>
-          <Table.HeaderCell>Amount</Table.HeaderCell>
-          <Table.HeaderCell>Total</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
+//   <div>
+//     <CartStepHeader />
+//     <Table striped celled>
+//       <Table.Header>
+//         <Table.Row>
+//           <Table.HeaderCell>Item</Table.HeaderCell>
+//           <Table.HeaderCell>Unit Price</Table.HeaderCell>
+//           <Table.HeaderCell>Amount</Table.HeaderCell>
+//           <Table.HeaderCell>Total</Table.HeaderCell>
+//         </Table.Row>
+//       </Table.Header>
 
-      <Table.Body>
-        {purchases.map(p =>
-          <Table.Row>
-            <Table.Cell>{p.item.name}</Table.Cell>
-            <Table.Cell>{p.unitPrice}€</Table.Cell>
-            <Table.Cell><Amount amount={p.amount}/></Table.Cell>
-            <Table.Cell>{p.total}€</Table.Cell>
-          </Table.Row>
-        )}
-    </Table.Body>
-  </Table>
-  <Button icon labelPosition="right" floated="right">
-    Buy
-      <Icon name="chevron right" />
-  </Button>
-  </div >
+//       <Table.Body>
+//         {purchases.map(p =>
+//           <Table.Row>
+//             <Table.Cell>{p.item.name}</Table.Cell>
+//             <Table.Cell>{p.unitPrice}€</Table.Cell>
+//             <Table.Cell><Amount amount={p.amount}/></Table.Cell>
+//             <Table.Cell>{p.total}€</Table.Cell>
+//           </Table.Row>
+//         )}
+//     </Table.Body>
+//   </Table>
+//   <Button icon labelPosition="right" floated="right">
+//     Buy
+//       <Icon name="chevron right" />
+//   </Button>
+//   </div >
+<ShoppingCart />
 
 );
