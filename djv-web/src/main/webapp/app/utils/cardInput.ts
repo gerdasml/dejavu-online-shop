be8114576f5ea5ef56e804552662f85e3044cd3d
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-04-18 19:49:02 +0300

    Extracted random numbers to constants to make code more readable

--- djv-web/src/main/webapp/app/utils/cardInput.ts
+++ djv-web/src/main/webapp/app/utils/cardInput.ts
@@ -1,16 +1,23 @@
 const clearNumber = (num: string) => num.replace(/\D+/g, "");
 
 export const formatCardNumber = (num: string) => {
-    if(!num) return num;
+    const NUMBER_LENGTH = 16;
+    const PART_LENGTH = 4;
+
+    if(num === undefined || num === null) return num;
     const clear = clearNumber(num);
-    const newNum = `${clear.slice(0, 4)} ${clear.slice(4, 8)} ${clear.slice(8, 12)} ${clear.slice(12, 16)}`;
+    let newNum = "";
+    for(let i = 0; i < NUMBER_LENGTH; i += PART_LENGTH) {
+        newNum += clear.slice(i, i + PART_LENGTH) + " ";
+    }
     return newNum.trim();
 };
 
 export const formatExpirationDate = (val: string) => {
+    const MONTH_LENGTH = 2;
     const clear = clearNumber(val);
-    if(clear.length >= 3) {
-        return `${clear.slice(0, 2)}/${clear.slice(2, 4)}`;
+    if(clear.length > MONTH_LENGTH) {
+        return `${clear.slice(0, MONTH_LENGTH)}/${clear.slice(MONTH_LENGTH, MONTH_LENGTH + MONTH_LENGTH)}`;
     }
     return clear;
 };
