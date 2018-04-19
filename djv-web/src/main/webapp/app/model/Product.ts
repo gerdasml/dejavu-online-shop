1f4af750e4735b5bb0b680a545a3aae508f05f1d
Gerda Šimoliūnaitė <sml.gerda@gmail.com>
2018-04-19 21:23:54 +0300

    Changed carousel design

--- djv-web/src/main/webapp/app/model/Product.ts
+++ djv-web/src/main/webapp/app/model/Product.ts
@@ -1,7 +1,10 @@
+import { IProductProperties } from "./ProductProperties";
+
 export interface IProduct {
     name: string;
     imageUrl: string;
     description: string;
     price: number;
+    properties: IProductProperties[];
     // TODO: update when product functionality is finished in back end
 }
