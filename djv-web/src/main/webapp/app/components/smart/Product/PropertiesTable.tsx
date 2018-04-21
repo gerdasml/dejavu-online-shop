64f549bb42e0dfc2b1a2539ebde2d47459d3b6b6
Gerda Šimoliūnaitė <sml.gerda@gmail.com>
2018-04-21 20:29:24 +0300

    Styled properties table so now it's all over the window

--- djv-web/src/main/webapp/app/components/smart/Product/PropertiesTable.tsx
+++ djv-web/src/main/webapp/app/components/smart/Product/PropertiesTable.tsx
@@ -3,6 +3,7 @@ import { Icon, Table } from "semantic-ui-react";
 
 import { products } from "../../../data/products";
 import { Product } from "../../../model/Product";
+import { ProductProperties } from "../../../model/ProductProperties";
 
 interface PropertiesTableProps { product: Product; }
 
@@ -10,14 +11,26 @@ export class PropertiesTable extends React.Component<PropertiesTableProps, {}> {
     constructor (props: PropertiesTableProps) {
         super(props);
     }
+    buildCell = (x?: ProductProperties) =>
+        x === undefined
+        ? <Table.Cell/>
+        : <Table.Cell title={x.name + ": " + x.value}><span style={{color:"blue", marginRight:"10px"}}>{x.name}:</span>
+        {x.value}</Table.Cell>
+
     render () {
+        const pairs = this.props.product.properties.reduce((res, val, idx, a) => {
+            if(idx % 2 === 0) {
+                res.push(a.slice(idx, idx+2));
+            }
+            return res;
+        }, []);
         return(
             <Table celled fixed singleLine>
                 <Table.Body>
-                    {this.props.product.properties.map((x, i) =>
+                    {pairs.map((x, i) =>
                     <Table.Row key={i}>
-                        <Table.Cell>{x.name}</Table.Cell>
-                        <Table.Cell>{x.value}</Table.Cell>
+                        {this.buildCell(x[0])}
+                        {this.buildCell(x[1])}
                     </Table.Row>)}
                 </Table.Body>
             </Table>
