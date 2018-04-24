15a41e0a21899a271fbcee1eb00ff989674fe01d
Mantas Petrikas <man.petrikas@gmail.com>
2018-04-24 14:09:04 +0300

    optimized imports

--- djv-product/src/main/java/lt/dejavu/product/api/CategoryApi.java
+++ djv-product/src/main/java/lt/dejavu/product/api/CategoryApi.java
@@ -6,14 +6,7 @@ import lt.dejavu.product.model.rest.request.CategoryRequest;
 import lt.dejavu.product.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
