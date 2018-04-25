4415d1a5aa5c19a1c3f29bc8a091d03740e92e18
Gerda Šimoliūnaitė <sml.gerda@gmail.com>
2018-04-25 19:29:56 +0300

    Added connection in login with backend

--- djv-web/src/main/webapp/app/components/dumb/Login/Login.tsx
+++ djv-web/src/main/webapp/app/components/dumb/Login/Login.tsx
@@ -1,34 +1,107 @@
 import * as React from "react";
-import { Button, Form, Grid, Icon, Modal, Segment } from "semantic-ui-react";
+import { Button, Form, Grid, Icon, Message, Modal, Segment } from "semantic-ui-react";
+import * as api from "../../../api";
+import { storeToken } from "../../../utils/token";
 import {Register} from "./Register";
 
 import "../../../../style/login.css";
 
+interface LoginState {
+    loading: boolean;
+    email: string;
+    password: string;
+    error: string;
+    open: boolean;
+}
+
 export class Login extends React.Component {
+    state = {
+        email: "",
+        error: "",
+        loading: false,
+        open: false,
+        password: ""
+    };
+    handleEmailInput = (event: React.FormEvent<HTMLInputElement>) => {
+        const value = event.currentTarget.value;
+        this.setState({
+            ...this.state, email: value
+        });
+    }
+    handlePasswordInput = (event: React.FormEvent<HTMLInputElement>) => {
+        const value = event.currentTarget.value;
+        this.setState({
+            ...this.state, password: value
+        });
+    }
+    handleOpen = () => this.setState({...this.state, open: true});
+    handleClose = () => this.setState({...this.state, open: false});
+    login = async (): Promise<void> => {
+        this.setState({
+            ...this.state, loading: true
+        });
+        const tkn = await api.auth.login(this.state.email, this.state.password);
+        if(api.isError(tkn)) {
+            this.setState({
+                ...this.state, error: tkn.message, loading: false
+            });
+            return undefined;
+        }
+        if(api.auth.isBanned(tkn)) {
+            this.setState({
+                ...this.state, error: "You are banned", loading: false
+            });
+            return undefined;
+        }
+        storeToken(tkn.token);
+        this.setState({
+            ...this.state, error: "", loading: false
+        });
+        this.handleClose();
+    }
     render () {
         return (
             <Modal trigger={<Button icon
                                     size="medium"
+                                    onClick={this.handleOpen.bind(this)}
             >
                 LOG&nbsp;IN
                 <br/>
                 <Icon name="user circle outline" size="big"/>
-            </Button>}>
+            </Button>}
+            open={this.state.open}
+            onClose={this.handleClose.bind(this)}
+            >
                 <Modal.Content id="loginModal">
                     <Grid columns={2} divided>
                         <Grid.Column>
                             <Segment basic>
                                 <h3> Already registered user?<br/>Log in:</h3>
-                                <Form>
+                                <Form loading={this.state.loading} error={this.state.error !== ""}>
                                     <Form.Field>
                                         <label>Email address</label>
-                                        <input type="email" placeholder="email"/>
+                                        <input
+                                            type="email"
+                                            placeholder="email"
+                                            onChange={this.handleEmailInput.bind(this)}
+                                        />
                                     </Form.Field>
                                     <Form.Field>
                                         <label>Password</label>
-                                        <input type="password" placeholder="********"/>
+                                        <input
+                                            type="password"
+                                            placeholder="********"
+                                            onChange={this.handlePasswordInput.bind(this)}
+                                        />
                                     </Form.Field>
-                                    <Button>Log in</Button>
+                                    <Message
+                                        error
+                                        header="Login failed"
+                                        content={this.state.error}
+                                    />
+                                    <Button onClick={this.login.bind(this)}>
+                                        Log in
+                                    </Button>
                                 </Form>
                             </Segment>
                         </Grid.Column>
