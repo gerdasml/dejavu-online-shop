0fa858b33e5390229988b2da501ccaced5905038
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-04-29 16:40:45 +0300

    Implemented Cart repository

--- djv-cart/src/main/java/lt/dejavu/cart/repository/CartRepository.java
+++ djv-cart/src/main/java/lt/dejavu/cart/repository/CartRepository.java
@@ -1,16 +1,24 @@
 package lt.dejavu.cart.repository;
 
+import lt.dejavu.auth.model.db.User;
 import lt.dejavu.cart.model.db.Cart;
 import lt.dejavu.order.model.db.OrderItem;
+import lt.dejavu.product.model.Product;
+
+import java.util.Optional;
 
 public interface CartRepository {
     Cart getCartByUserId(long userId);
 
-    OrderItem getOrderItemByProductId(Cart cart, long productId);
+    Optional<OrderItem> getOrderItemByProductId(Cart cart, long productId);
 
     void removeItem(Cart cart, long productId);
 
-    Cart createEmptyCart(long userId);
+    void addOrderItem(Cart cart, Product product, int amount);
+
+    void updateOrderItem(OrderItem item);
+
+    Cart createEmptyCart(User user);
 
     void deleteCart(long userId);
 }
--- djv-cart/src/main/java/lt/dejavu/cart/service/CartServiceImpl.java
+++ djv-cart/src/main/java/lt/dejavu/cart/service/CartServiceImpl.java
@@ -7,17 +7,24 @@ import lt.dejavu.cart.mapper.CartMapper;
 import lt.dejavu.cart.model.db.Cart;
 import lt.dejavu.cart.repository.CartRepository;
 import lt.dejavu.order.model.db.OrderItem;
+import lt.dejavu.product.exception.ProductNotFoundException;
+import lt.dejavu.product.model.Product;
+import lt.dejavu.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CartServiceImpl implements CartService {
     private final CartRepository cartRepository;
+    private final ProductRepository productRepository;
     private final CartMapper cartMapper;
 
     @Autowired
-    public CartServiceImpl(CartRepository cartRepository, CartMapper cartMapper) {
+    public CartServiceImpl(CartRepository cartRepository, ProductRepository productRepository, CartMapper cartMapper) {
         this.cartRepository = cartRepository;
+        this.productRepository = productRepository;
         this.cartMapper = cartMapper;
     }
 
@@ -33,17 +40,23 @@ public class CartServiceImpl implements CartService {
         if(cartRepository.getOrderItemByProductId(cart, productId) != null) {
             throw new ProductAlreadyInCartException("This product is already in your cart");
         }
-        // TODO: add product to cart
+        Product product = productRepository.getProduct(productId);
+        if(product == null) {
+            throw new ProductNotFoundException("The product with the specified ID was nto found");
+        }
+        cartRepository.addOrderItem(cart, product, amount);
     }
 
     @Override
     public void updateAmount(long userId, long productId, int amount) {
         Cart cart = cartRepository.getCartByUserId(userId);
-        OrderItem item = cartRepository.getOrderItemByProductId(cart, productId);
-        if(item == null) {
+        Optional<OrderItem> itemOpt = cartRepository.getOrderItemByProductId(cart, productId);
+        if(!itemOpt.isPresent()) {
             throw new ProductNotInCartException("This product is not in your cart");
         }
-        // TODO: update product amount
+        OrderItem item = itemOpt.get();
+        item.setAmount(amount);
+        cartRepository.updateOrderItem(item);
     }
 
     @Override
