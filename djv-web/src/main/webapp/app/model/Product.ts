6e79271afdf6cad8ca5051d5d86b319e60ae28c5
Mantas Petrikas <man.petrikas@gmail.com>
2018-04-29 14:17:21 +0300

    added display name - from now on it will be called identifier

--- djv-web/src/main/webapp/app/model/Product.ts
+++ djv-web/src/main/webapp/app/model/Product.ts
@@ -2,6 +2,7 @@ import { ProductProperties } from "./ProductProperties";
 
 export interface Product {
     id?: number;
+    identifier?: string;
     name: string;
     mainImageUrl?: string;
     additionalImagesUrls?: string[];
