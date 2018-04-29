158ac1dfce5e6fed83696a16e20f6b14ea30604b
Margiris Strakšys <margirisstr@gmail.com>
2018-04-29 11:34:34 +0300

    Merge confict fixes.

--- djv-web/src/main/webapp/app/components/views/Regular/Profile.tsx
+++ djv-web/src/main/webapp/app/components/views/Regular/Profile.tsx
@@ -2,10 +2,10 @@ import * as React from "react";
 
 import {Grid, Header} from "semantic-ui-react";
 
-import "../../../style/profile.css";
+import "../../../../style/profile.css";
 
-import {OrderHistory} from "../dumb/Profile/OrderHistory";
-import {ProfileInfo} from "../dumb/Profile/ProfileInfo";
+import {OrderHistory} from "../../dumb/Profile/OrderHistory";
+import {ProfileInfo} from "../../dumb/Profile/ProfileInfo";
 
 export class Profile extends React.Component<{},{}> {
     render () {
