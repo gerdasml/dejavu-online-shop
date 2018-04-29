bd4c5293a264850f92db6cb7090f78ce1012ef03
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-04-29 14:30:06 +0300

    Added cart DB entity

---
+++ djv-cart/src/main/java/lt/dejavu/cart/model/db/Cart.java
@@ -0,0 +1,29 @@
+package lt.dejavu.cart.model.db;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lt.dejavu.auth.model.db.User;
+import lt.dejavu.order.model.db.OrderItem;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "cart")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @OneToMany
+    @JoinTable(name = "cartItem")
+    private List<OrderItem> items;
+}
