7d817e3f117d6d3a9552bce1393cd63cffb9210b
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-05-01 21:19:16 +0300

    Implemented cart checkout

--- djv-cart/src/main/java/lt/dejavu/cart/service/CartService.java
+++ djv-cart/src/main/java/lt/dejavu/cart/service/CartService.java
@@ -1,6 +1,9 @@
 package lt.dejavu.cart.service;
 
+import lt.dejavu.auth.model.db.Address;
 import lt.dejavu.cart.dto.CartDto;
+import lt.dejavu.payment.exception.PaymentException;
+import lt.dejavu.payment.model.Card;
 
 public interface CartService {
     CartDto getCart(long userId);
@@ -10,4 +13,6 @@ public interface CartService {
     void updateAmount(long userId, long productId, int amount);
 
     void removeProduct(long userId, long productId);
+
+    void checkout(long userId, Card cardInfo, Address shippingAddress) throws PaymentException;
 }
