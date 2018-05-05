940c12581c5eb63a89750dba7a24b72312fb8d93
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-05-06 00:01:47 +0300

    Moved image upload to a separate file

--- djv-web/src/main/webapp/app/components/views/Admin/Admin.tsx
+++ djv-web/src/main/webapp/app/components/views/Admin/Admin.tsx
@@ -1,58 +1,12 @@
 import * as React from "react";
 
-import { Button, Icon, message, Modal, Upload } from "antd";
-import { RcFile } from "antd/lib/upload/interface";
 import {Header} from "semantic-ui-react";
-import * as api from "../../../api";
 
-import { dataUriToFile } from "../../../utils/file";
-
-import { ImageCropper } from "../Admin/product/ImageCropper";
-
-const upload = async (info: any) => {
-    const img = info.file;
-    const response = await api.image.uploadImage(img);
-    if (api.isError(response)) {
-        info.onError(undefined, response.message);
-        return;
-    }
-    info.onSuccess(response);
-};
-
-const cloneFile = (old: RcFile, newData: string) => {
-    const f = dataUriToFile(newData, old) as RcFile;
-    f.uid = old.uid;
-    return f;
-};
-
-const handleBefore = async (file: RcFile): Promise<RcFile> =>
-    new Promise<RcFile>((resolve, reject) => {
-        let newImg: string = file.toString();
-        const onChange = (f: string) => newImg = f;
-        Modal.confirm({
-            content: <ImageCropper onChange={onChange} image={file} />,
-            iconType: "none",
-            maskClosable: true,
-            style: { top: 0 },
-            width: "80%",
-            onOk () {
-                resolve(cloneFile(file, newImg));
-            },
-            onCancel () {
-                reject(file);
-            }
-        });
-    });
+import { ImageUpload } from "./product/ImageUpload";
 
 export const Admin = () => (
     <div>
         <Header size="large">Admin</Header>
-        <Upload onChange={info => console.log("CHANGED", info)}
-                beforeUpload={handleBefore}
-                customRequest={upload}
-                listType="picture-card">
-            <Icon type="plus" />
-            <div className="ant-upload-text">Upload</div>
-        </Upload>
+        <ImageUpload onUpdate={imgs => console.log(imgs)} />
     </div>
 );
---
+++ djv-web/src/main/webapp/app/components/views/Admin/product/ImageUpload.tsx
@@ -0,0 +1,77 @@
+import * as React from "react";
+
+import { Button, Icon, message, Modal, Upload } from "antd";
+import { RcFile, UploadChangeParam, UploadFile } from "antd/lib/upload/interface";
+import * as api from "../../../../api";
+
+import { dataUriToFile } from "../../../../utils/file";
+
+import { ImageInfo } from "../../../../model/ImageInfo";
+import { ImageCropper } from "../../Admin/product/ImageCropper";
+
+interface ImageUploadProps {
+    onUpdate: (files: ImageInfo[]) => void;
+}
+
+interface ImageUploadState {
+    images: UploadFile[];
+}
+
+const cloneFile = (old: RcFile, newData: string) => {
+    const f = dataUriToFile(newData, old) as RcFile;
+    f.uid = old.uid;
+    return f;
+};
+
+export class ImageUpload extends React.Component<ImageUploadProps, ImageUploadState> {
+    state: ImageUploadState = {
+        images: []
+    };
+
+    upload = async (info: any) => {
+        const img = info.file;
+        const response = await api.image.uploadImage(img);
+        if (api.isError(response)) {
+            info.onError(undefined, response.message);
+            return;
+        }
+        info.onSuccess(response);
+    }
+
+    handleChange = (info: UploadChangeParam) => {
+        const goodFiles = info.fileList.filter(f => f.status === "done"); // success?
+        this.props.onUpdate(goodFiles.map(x => x.response));
+        console.log(info);
+    }
+
+    handleBefore = async (file: RcFile): Promise<RcFile> =>
+        new Promise<RcFile>((resolve, reject) => {
+            let newImg: string = file.toString();
+            const onChange = (f: string) => newImg = f;
+            Modal.confirm({
+                content: <ImageCropper onChange={onChange} image={file} />,
+                iconType: "none",
+                maskClosable: true,
+                style: { top: 0 },
+                width: "80%",
+                onOk () {
+                    resolve(cloneFile(file, newImg));
+                },
+                onCancel () {
+                    reject(file);
+                }
+            });
+        })
+
+    render () {
+        return (
+            <Upload onChange={this.handleChange.bind(this)}
+                    beforeUpload={this.handleBefore.bind(this)}
+                    customRequest={this.upload.bind(this)}
+                    listType="picture-card">
+                <Icon type="plus" />
+                <div className="ant-upload-text">Upload</div>
+            </Upload>
+        );
+    }
+}
