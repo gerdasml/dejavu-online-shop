587e4072f83c5d8f897c73843eb4f71b3d3066f6
Gerda Šimoliūnaitė <sml.gerda@gmail.com>
2018-05-06 22:59:33 +0300

    Implemented row removing and adding

--- djv-web/src/main/webapp/app/components/views/Admin/product/ProductPropertiesTable.tsx
+++ djv-web/src/main/webapp/app/components/views/Admin/product/ProductPropertiesTable.tsx
@@ -26,21 +26,38 @@ export class ProductPropertiesTable extends React.Component<PropertiesTableProps
     state: PropertiesTableState = {
         properties: this.props.properties.map(addKey)
     };
+    handleAddRow () {
+        const p = this.state.properties;
+        const lastKey = p.length === 0 ? -1 : p[p.length-1].key;
+        const newProp: Property = {name: "", value: "", key: lastKey+1};
+        this.setState({properties: [...p, newProp]});
+    }
+    handleRemoveRow (keyToDelete: number) {
+        const newProp = this.state.properties.filter(x => x.key !== keyToDelete);
+        this.setState({properties: newProp});
+    }
     render () {
         return (
-            <PropertiesTable pagination={false} dataSource={this.state.properties}>
-                <PropertyColumn
-                    key="name"
-                    title="Name"
-                    render={(text, record, index) => <EditableCell value={record.name}/>} />
-                <PropertyColumn
-                    key="value"
-                    title="Value"
-                    render={(text, record, index) => <EditableCell value={record.value}/>} />
-                <PropertyColumn
-                    key="remove"
-                    render={(_, record) => <Button type="danger"><Icon type="delete" /></Button>}/>
-            </PropertiesTable>
+            <div>
+                <PropertiesTable pagination={false} dataSource={this.state.properties}>
+                    <PropertyColumn
+                        key="name"
+                        title="Name"
+                        render={(text, record, index) => <EditableCell value={record.name}/>} />
+                    <PropertyColumn
+                        key="value"
+                        title="Value"
+                        render={(text, record, index) => <EditableCell value={record.value}/>} />
+                    <PropertyColumn
+                        key="remove"
+                        render={(_, record) => <Button
+                                                    type="danger"
+                                                    onClick={() => this.handleRemoveRow(record.key)}>
+                                                    <Icon type="delete" />
+                                                </Button>}/>
+                </PropertiesTable>
+                <Button onClick={this.handleAddRow.bind(this)}>Add new property</Button>
+            </div>
         );
     }
 }
