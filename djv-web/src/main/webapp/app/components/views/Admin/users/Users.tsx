62e8125431ad6b995e725dad2455143f509591ea
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-05-06 14:07:30 +0300

    Started implementation of users view

--- djv-web/src/main/webapp/app/components/views/Admin/users/Users.tsx
+++ djv-web/src/main/webapp/app/components/views/Admin/users/Users.tsx
@@ -2,6 +2,50 @@ import * as React from "react";
 
 import {Header} from "semantic-ui-react";
 
+import { Spin } from "antd";
+
+import { OrderSummary } from "../../../../model/Order";
+
+import * as api from "../../../../api";
+
 export const Users = () => (
     <Header size="large">Users</Header>
 );
+
+interface UsersState {
+    isLoading: boolean;
+    summaries: OrderSummary[];
+}
+
+export class Users extends React.Component<never, UsersState> {
+    state: UsersState = {
+        isLoading: true,
+        summaries: [],
+    };
+
+    async componentWillMount () {
+        const response = await api.order.getOrderSummary();
+        if (api.isError(response)) {
+            return;
+        }
+        this.setState({summaries: response, isLoading: false});
+    }
+
+    renderLoading () {
+        return (
+            <Spin size="large" />
+        );
+    }
+
+    renderLoaded () {
+
+    }
+    render () {
+        const { isLoading } = this.state;
+        return (
+            <div>
+                { isLoading ? this.renderLoading() : this.renderLoaded() }
+            </div>
+        );
+    }
+}
