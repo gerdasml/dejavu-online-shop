387c47c55c9da5db00d4c1c80e64315714f32900
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-05-06 15:10:43 +0300

    Started implementing single user admin view

---
+++ djv-web/src/main/webapp/app/components/views/Admin/users/SingleUser.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import { RouteComponentProps } from "react-router-dom";
+
+import { notification, Spin } from "antd";
+import { User } from "../../../../model/User";
+
+import * as api from "../../../../api";
+import { Order } from "../../../../model/Order";
+
+interface SingleUserProps {
+    id: number;
+}
+
+interface SingleUserState {
+    isLoading: boolean;
+    user?: User;
+    orders?: Order[];
+}
+
+export class SingleUser extends React.Component<RouteComponentProps<SingleUserProps>, SingleUserState> {
+    state: SingleUserState = {
+        isLoading: true
+    };
+
+    async componentDidMount () {
+        const userResponse = await api.user.getUser(this.props.match.params.id);
+        if (api.isError(userResponse)) {
+            notification.error({message: "Failed to fetch user data", description: userResponse.message});
+            console.error(userResponse);
+            return;
+        }
+        const ordersResponse = await api.order.getUserOrderHistory(this.props.match.params.id);
+        if (api.isError(ordersResponse)) {
+            notification.error({message: "Failed to fetch orders data", description: ordersResponse.message});
+            console.error(ordersResponse);
+            return;
+        }
+
+        this.setState({user: userResponse, orders: ordersResponse, isLoading: false});
+    }
+
+    render () {
+        const { isLoading, user, orders } = this.state;
+        return (
+            <Spin spinning={isLoading} size="large">
+                <p>{JSON.stringify(user)}</p>
+                <p>{JSON.stringify(orders)}</p>
+            </Spin>
+        );
+    }
+}
