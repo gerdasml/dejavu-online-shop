192f9ef9c42e8ec34d42319ca38a01324b4be828
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-05-06 13:54:37 +0300

    Added order summary endpoint in backend

--- djv-order/src/main/java/lt/dejavu/order/service/OrderServiceImpl.java
+++ djv-order/src/main/java/lt/dejavu/order/service/OrderServiceImpl.java
@@ -1,9 +1,11 @@
 package lt.dejavu.order.service;
 
+import lt.dejavu.auth.mapper.UserMapper;
 import lt.dejavu.auth.model.db.User;
 import lt.dejavu.auth.repository.UserRepository;
 import lt.dejavu.order.dto.OrderDto;
 import lt.dejavu.order.dto.OrderItemDto;
+import lt.dejavu.order.dto.OrderSummaryDto;
 import lt.dejavu.order.exception.OrderNotFoundException;
 import lt.dejavu.order.mapper.OrderMapper;
 import lt.dejavu.order.model.OrderStatus;
@@ -14,6 +16,8 @@ import lt.dejavu.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -24,13 +28,15 @@ public class OrderServiceImpl implements OrderService {
     private final OrderMapper orderMapper;
     private final UserRepository userRepository;
     private final ProductRepository productRepository;
+    private final UserMapper userMapper;
 
     @Autowired
-    public OrderServiceImpl(OrderRepository orderRepository, OrderMapper orderMapper, UserRepository userRepository, ProductRepository productRepository) {
+    public OrderServiceImpl(OrderRepository orderRepository, OrderMapper orderMapper, UserRepository userRepository, ProductRepository productRepository, UserMapper userMapper) {
         this.orderRepository = orderRepository;
         this.orderMapper = orderMapper;
         this.userRepository = userRepository;
         this.productRepository = productRepository;
+        this.userMapper = userMapper;
     }
 
     @Override
@@ -46,7 +52,7 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public OrderDto getOrderById(long orderId) {
         Order order = orderRepository.getOrder(orderId);
-        if(order == null) {
+        if (order == null) {
             throw new OrderNotFoundException("The order with the specified ID was not found");
         }
         return orderMapper.map(order);
@@ -67,14 +73,38 @@ public class OrderServiceImpl implements OrderService {
         order.setCreationDate(dto.getCreatedDate());
         order.setShippingAddress(dto.getShippingAddress());
         List<OrderItem> orderItems = dto.getItems()
-                                     .stream()
-                                     .map(item -> buildOrderItem(item, order))
-                                     .collect(toList());
+                                        .stream()
+                                        .map(item -> buildOrderItem(item, order))
+                                        .collect(toList());
 
         order.setItems(orderItems);
 
         orderRepository.saveOrder(order);
         return order.getId();
+    }
+
+    @Override
+    public List<OrderSummaryDto> getOrderSummary() {
+        List<User> users = userRepository.getAllUsers();
+        return users.stream().map(this::buildOrderSummary).collect(toList());
+    }
+
+    private OrderSummaryDto buildOrderSummary(User user) {
+        List<OrderDto> orders = this.getUserOrderHistory(user.getId());
+        int count = orders.size();
+        BigDecimal total = orders.stream().map(OrderDto::getTotal).reduce(BigDecimal.ZERO, BigDecimal::add);
+        BigDecimal average =
+                count == 0
+                ? null
+                : total.divide(BigDecimal.valueOf(count), 2, RoundingMode.HALF_UP);
+
+        OrderSummaryDto orderSummary = new OrderSummaryDto();
+        orderSummary.setUser(userMapper.map(user));
+        orderSummary.setOrderCount(count);
+        orderSummary.setTotalSpending(total);
+        orderSummary.setAverageSpending(average);
+
+        return orderSummary;
     }
 
     private OrderItem buildOrderItem(OrderItemDto dto, Order order) {
