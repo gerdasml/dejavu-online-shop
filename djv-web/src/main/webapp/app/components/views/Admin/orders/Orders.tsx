26fdbad1c7eabe146529e78b45bcc2e3f8c547a0
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-05-06 18:21:16 +0300

    Added Orders view

--- djv-web/src/main/webapp/app/components/views/Admin/orders/Orders.tsx
+++ djv-web/src/main/webapp/app/components/views/Admin/orders/Orders.tsx
@@ -1,7 +1,38 @@
 import * as React from "react";
 
-import {Header} from "semantic-ui-react";
+import { notification, Spin } from "antd";
 
-export const Orders = () => (
-    <Header size="large">Orders</Header>
-);
+import { Order } from "../../../../model/Order";
+import { OrdersTable } from "../common/Table/OrdersTable";
+
+import * as api from "../../../../api";
+
+interface OrdersState {
+    isLoading: boolean;
+    orders: Order[];
+}
+
+export class Orders extends React.Component<never, OrdersState> {
+    state: OrdersState = {
+        isLoading: true,
+        orders: [],
+    };
+
+    async componentWillMount () {
+        const response = await api.order.getAllOrders();
+        if (api.isError(response)) {
+            notification.error({message: "Failed to fetch data", description: response.message});
+            return;
+        }
+        this.setState({orders: response, isLoading: false});
+    }
+
+    render () {
+        const { isLoading } = this.state;
+        return (
+            <Spin spinning={isLoading} size="large">
+                <OrdersTable orders={this.state.orders} />
+            </Spin>
+        );
+    }
+}
