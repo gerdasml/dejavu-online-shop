3d69cc81e0cb208b387bf29162b7d8e73340f9a7
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-05-09 18:12:28 +0300

    Added icon selection component

---
+++ djv-web/src/main/webapp/app/components/views/Admin/categories/IconSelect.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+import { Button, Card, Collapse } from "antd";
+import { Icon, SemanticICONS } from "semantic-ui-react";
+
+import { icons, IconSet } from "../../../../data/icons";
+
+interface IconSelectProps {
+    onSelect: (iconName: string) => void;
+}
+
+const mapIcon = (icon: string, key: number, props: IconSelectProps) => (
+    <Button size="small" key={key} onClick={() => props.onSelect(icon)}><Icon name={icon as SemanticICONS} /></Button>
+);
+
+const mapIconSet = (set: IconSet, key: number, props: IconSelectProps) => (
+    <Collapse.Panel header={set.name} key={key.toString()}>
+        {set.values.map((i, idx) => mapIcon(i, idx, props))}
+    </Collapse.Panel>
+);
+
+export const IconSelect = (props: IconSelectProps) => (
+    <Collapse>
+        {icons.map((i, idx) => mapIconSet(i, idx, props))}
+    </Collapse>
+);
