14d95d50392c1560ead532ac51ca8a6bdbdb8add
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-05-09 15:25:57 +0300

    Added update category api function

--- djv-web/src/main/webapp/app/api/category.ts
+++ djv-web/src/main/webapp/app/api/category.ts
@@ -1,5 +1,6 @@
 import { ApiResponse } from "./ApiResponse";
 
+import { Category } from "../model/Category";
 import { CategoryTree } from "../model/CategoryTree";
 import { fetchData, HttpMethod } from "./utils";
 
@@ -7,3 +8,6 @@ const PATH_PREFIX = "/api/category";
 
 export const getCategoryTree = (): Promise<ApiResponse<CategoryTree[]>> =>
     fetchData(PATH_PREFIX + "/categoryTree", HttpMethod.GET);
+
+export const updateCategory = (id: number, category: Category): Promise<ApiResponse<void>> =>
+    fetchData(PATH_PREFIX + "/" + id.toString(), HttpMethod.PUT, category);
