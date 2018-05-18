9c1102ad4c7107172152e1807a650e3f7369decb
Margiris Strakšys <margirisstr@gmail.com>
2018-05-18 12:39:21 +0300

    Order delivery info page changed to have shipping address fields only. Removed ratings from orderHistory table.

--- djv-web/src/main/webapp/app/components/dumb/Cart/Steps/DeliveryInfo.tsx
+++ djv-web/src/main/webapp/app/components/dumb/Cart/Steps/DeliveryInfo.tsx
@@ -10,82 +10,22 @@ import { User } from "../../../../model/User";
 
 interface DeliveryInfoProps {
     onStepComplete: () => void;
-    onUserInfoChange: (user: User) => void;
-    user: User;
+    onShippingInfoChange: (address: Address) => void;
+    shippingAddress: Address;
 }
 export class DeliveryInfo extends React.Component <DeliveryInfoProps, {}> {
+
     handleNewAddress = (newAddress: Address) => {
-        const newUser = this.props.user;
-        newUser.address = newAddress;
-        this.props.onUserInfoChange(newUser);
-    }
-    handleFirstNameChange = (event: React.FormEvent<HTMLInputElement>) => {
-        const value = event.currentTarget.value;
-        const newUser = { ...this.props.user };
-        newUser.firstName = value;
-        this.props.onUserInfoChange(newUser);
-    }
-    handleLastNameChange = (event: React.FormEvent<HTMLInputElement>) => {
-        const value = event.currentTarget.value;
-        const newUser = { ...this.props.user };
-        newUser.lastName = value;
-        this.props.onUserInfoChange(newUser);
-    }
-    handleEmailChange = (event: React.FormEvent<HTMLInputElement>) => {
-        const value = event.currentTarget.value;
-        const newUser = { ...this.props.user };
-        newUser.email = value;
-        this.props.onUserInfoChange(newUser);
-    }
-    handlePhoneChange = (event: React.FormEvent<HTMLInputElement>) => {
-        const value = event.currentTarget.value;
-        const newUser = { ...this.props.user };
-        newUser.phone = value;
-        this.props.onUserInfoChange(newUser);
+        const address = newAddress;
+        this.props.onShippingInfoChange(address);
     }
 
     render () {
         return (
             <Form size = "big" id="deliveryInfoForm">
-                <Form.Field inline>
-                    <label>First name</label>
-                    <input
-                        type="text"
-                        placeholder="John"
-                        defaultValue={this.props.user.firstName}
-                        onChange={this.handleFirstNameChange.bind(this)}
-                    />
-                </Form.Field>
-                <Form.Field inline>
-                    <label>Last name</label>
-                    <input
-                        type="text"
-                        placeholder="Doe"
-                        defaultValue={this.props.user.lastName}
-                        onChange={this.handleLastNameChange.bind(this)}
-                />
-                </Form.Field>
-                <Form.Field inline>
-                    <label>Email</label>
-                    <input
-                        type="email"
-                        placeholder="email@email.com"
-                        defaultValue={this.props.user.email}
-                        onChange={this.handleEmailChange.bind(this)}
-                    />
-                </Form.Field>
-                <Form.Field inline>
-                    <label>Phone number</label>
-                    <input
-                        type="text"
-                        placeholder="+37061234567"
-                        defaultValue={this.props.user.phone}
-                        onChange={this.handlePhoneChange.bind(this)}
-                />
-                </Form.Field>
                 <AddressInput
                     formSize="big"
-                    address={this.props.user.address}
+                    address={this.props.shippingAddress}
                     onAddressChange={ newAddress => this.handleNewAddress(newAddress)} />
                 <Button type="submit" positive onClick={this.props.onStepComplete}>Next</Button>
             </Form>
