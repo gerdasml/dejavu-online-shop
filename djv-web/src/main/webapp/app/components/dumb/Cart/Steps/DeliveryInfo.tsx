472fee30dcec160051f4f4aa3f955772c5ca2d26
SalGabriele <gabriele.saletyte@gmail.com>
2018-05-20 10:50:17 +0300

    fixed merge conflict

--- djv-web/src/main/webapp/app/components/dumb/Cart/Steps/DeliveryInfo.tsx
+++ djv-web/src/main/webapp/app/components/dumb/Cart/Steps/DeliveryInfo.tsx
@@ -32,30 +32,3 @@ export class DeliveryInfo extends React.Component <DeliveryInfoProps, {}> {
         );
     }
 }
-<<<<<<< HEAD
-export const DeliveryInfo = (props: DeliveryInfoProps) => (
-    <div>
-        <Form size = "big" id="deliveryInfoForm">
-            <Form.Field inline>
-                <label>First name</label>
-                <input type="text" value="First Name"/>
-            </Form.Field>
-            <Form.Field inline>
-                <label>Last name</label>
-                <input type="text" value="Last Name" />
-            </Form.Field>
-            <Form.Field inline>
-                <label>Email</label>
-                <input type="email" value="email@email.com" />
-            </Form.Field>
-            <Form.Field inline>
-                <label>Phone number</label>
-                <input type="text" value="+370********" />
-            </Form.Field>
-            <AddressInput formSize="big" address={{}} onAddressChange={()=> { }} />
-            <Button type="submit" positive onClick={props.onStepComplete}>Next</Button>
-        </Form>
-    </div >
-);
-=======
->>>>>>> master
