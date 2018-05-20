e189978121bc912551800a85b22a1320e1ec788b
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-05-20 14:04:08 +0300

    Fixed wrong api method url

--- djv-web/src/main/webapp/app/api/category.ts
+++ djv-web/src/main/webapp/app/api/category.ts
@@ -7,7 +7,7 @@ import { fetchData, HttpMethod } from "./utils";
 const PATH_PREFIX = "/api/category";
 
 export const getCategoryByIdentifier = (identifier: string): Promise<ApiResponse<Category>> =>
-    fetchData(PATH_PREFIX + "/byIdentifier?identifier=" + identifier, HttpMethod.GET);
+    fetchData(PATH_PREFIX + "/?identifier=" + identifier, HttpMethod.GET);
 
 export const getCategoryTree = (): Promise<ApiResponse<CategoryTree[]>> =>
     fetchData(PATH_PREFIX + "/categoryTree", HttpMethod.GET);
