3d75c0d7a2ebf49115c97869eec48e3361e6580a
SalGabriele <gabriele.saletyte@gmail.com>
2018-05-21 16:58:13 +0300

    created discounds display

--- djv-web/src/main/webapp/app/components/dumb/Home/ProductCard.tsx
+++ djv-web/src/main/webapp/app/components/dumb/Home/ProductCard.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 
-import {Button, Card, Header, Icon, Image, Label} from "semantic-ui-react";
+import { Button, Card, Grid, Header, Icon, Image, Label } from "semantic-ui-react";
 
 import { Product } from "../../../model/Product";
-
 import { NavLink } from "react-router-dom";
 import { shortenString } from "../../../utils/common";
+import { isNullOrUndefined } from "util";
+
+import "../../../../style/productCard.css";
 
 interface ProductCardProps {
     product: Product;
@@ -13,33 +15,68 @@ interface ProductCardProps {
 }
 
 export const ProductCard = (props: ProductCardProps) => (
-        <Card>
-            <NavLink to={`/product/${props.product.identifier}`}>
-                <Header attached>{props.product.price}€</Header>
-                <Image src={props.product.mainImageUrl} />
-                <Card.Content>
-                    <Card.Header>
-                        {props.product.name}
-                    </Card.Header>
-                    <Card.Description>
-                        {shortenString(props.product.description)}
-                    </Card.Description>
-                </Card.Content>
-            </NavLink>
-            <Card.Content extra textAlign="center">
-            <Label pointing="right">Add to cart: </Label>
-                <Button
-                    positive
-                    animated="fade"
-                    onClick={() => props.onProductAddToCart(props.product)}
-                >
-                    <Button.Content visible>
-                        <Icon name="shop"/>
-                    </Button.Content>
-                    <Button.Content hidden>
-                        <Icon name="plus"/>1
-                    </Button.Content>
-                </Button>
-            </Card.Content>
-        </Card>
+    <Card as={NavLink} to={`/product/${props.product.identifier}`}>
+        <Header as="h3" attached >{props.product.name}</Header>
+        {isNullOrUndefined(props.product.discount)
+        ?
+            <Image src={props.product.mainImageUrl} />
+        :
+        props.product.discount.type === "PERCENTAGE"
+        ?
+            <Image src={props.product.mainImageUrl}
+                label={{
+                as: "a",
+                color: "orange",
+                ribbon: "right",
+                size: "large",
+                icon: "percent",
+                content: props.product.discount.value
+                }} />
+        :
+        <Image src={props.product.mainImageUrl}
+            label={{
+            as: "a",
+            color: "orange",
+            ribbon: "right",
+            size: "large",
+            icon: "euro",
+            content: "- " + props.product.discount.value
+            }} />
+
+        }
+        <Card.Content>
+            <Card.Description>
+                {shortenString(props.product.description)}
+            </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+            <Grid columns={2}>
+                <Grid.Column>
+                    {isNullOrUndefined(props.product.discount)
+                    ?
+                        <Header as="h3" id="priceHeader">{props.product.price}€</Header>
+                        :
+                        <Header as="h3">{props.product.discount.finalPrice}€
+                            <Header.Subheader as="del" content={props.product.price + "€"} />
+                        </Header>
+                    }
+                </Grid.Column>
+                <Grid.Column>
+                    <Button
+                        floated="right"
+                        positive
+                        animated="fade"
+                        onClick={() => props.onProductAddToCart(props.product)}
+                    >
+                        <Button.Content visible>
+                            <Icon name="shop" />
+                        </Button.Content>
+                        <Button.Content hidden>
+                            <Icon name="plus" />
+                        </Button.Content>
+                    </Button>
+                </Grid.Column>
+            </Grid>
+        </Card.Content>
+    </Card>
 );
