d19f886012e0efca0fef9646525eaaaf84fb7870
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-05-21 20:41:58 +0300

    Implemented more repository methods

--- djv-discount/src/main/java/lt/dejavu/discount/repository/DiscountRepository.java
+++ djv-discount/src/main/java/lt/dejavu/discount/repository/DiscountRepository.java
@@ -2,6 +2,12 @@ package lt.dejavu.discount.repository;
 
 import lt.dejavu.discount.model.db.Discount;
 
+import java.util.List;
+
 public interface DiscountRepository {
     long addDiscount(Discount discount);
+    Discount getDiscount (long id);
+    void updateDiscount(long id, Discount newDiscount);
+    List<Discount> getAllDiscounts();
+    void deleteDiscount(long id);
 }
--- djv-discount/src/main/java/lt/dejavu/discount/repository/DiscountRepositoryImpl.java
+++ djv-discount/src/main/java/lt/dejavu/discount/repository/DiscountRepositoryImpl.java
@@ -1,11 +1,17 @@
 package lt.dejavu.discount.repository;
 
+import lt.dejavu.auth.model.db.User;
 import lt.dejavu.discount.model.db.Discount;
+import lt.dejavu.discount.model.db.ProductDiscount;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 @Repository
 @Transactional
@@ -16,5 +22,32 @@ public class DiscountRepositoryImpl implements DiscountRepository {
     public long addDiscount(Discount discount) {
         em.persist(discount);
         return discount.getId();
+    }
+
+    @Override
+    public Discount getDiscount(long id) {
+        return em.find(Discount.class, id);
+    }
+
+    @Override
+    public void updateDiscount(long id, Discount newDiscount) {
+        Discount oldDiscount = em.find(Discount.class, id);
+        newDiscount.setId(id);
+        em.merge(newDiscount);
+    }
+
+    @Override
+    public List<Discount> getAllDiscounts() {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Discount> cq = cb.createQuery(Discount.class);
+        Root<Discount> rootEntry = cq.from(Discount.class);
+        CriteriaQuery<Discount> all = cq.select(rootEntry);
+        return em.createQuery(all).getResultList();
+    }
+
+    @Override
+    public void deleteDiscount(long id) {
+        Discount discount = getDiscount(id);
+        em.remove(discount);
     }
 }
