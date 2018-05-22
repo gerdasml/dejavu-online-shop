c76ffacae3c0472f4e76fd25cc428422d02cb41b
Vytautas Strimaitis <vstrimaitis@gmail.com>
2018-05-22 22:42:36 +0300

    Fetch category info in category view

--- djv-web/src/main/webapp/app/components/views/Regular/Category.tsx
+++ djv-web/src/main/webapp/app/components/views/Regular/Category.tsx
@@ -4,7 +4,7 @@ import { Loader} from "semantic-ui-react";
 
 import { RouteComponentProps } from "react-router-dom";
 import {Product} from "../../../model/Product";
-import {Category as CategoryModel} from "../../../model/Category";
+import {Category as CategoryModel, CategoryInfo} from "../../../model/Category";
 
 import { notification } from "antd";
 import * as api from "../../../api";
@@ -17,8 +17,8 @@ interface CategoryRouteProps {
 interface CategoryState {
     products: Product[];
     category?: CategoryModel;
+    categoryInfo?: CategoryInfo;
     isLoading: boolean;
-    error?: string;
     activePage: number;
 }
 
@@ -26,7 +26,6 @@ const PRODUCTS_PER_PAGE = 20;
 
 export class Category extends React.Component<RouteComponentProps<CategoryRouteProps>, CategoryState> {
     state: CategoryState = {
-        error: "",
         isLoading: true,
         products: [],
         activePage: 1
@@ -50,18 +49,26 @@ export class Category extends React.Component<RouteComponentProps<CategoryRouteP
                 ]);
         if (api.isError(productResponse)) {
             notification.error({ message: "Failed to fetch product data", description: productResponse.message });
-            this.setState({ error: productResponse.message, isLoading: false });
+            this.setState({ ...this.state, isLoading: false });
             return;
         }
         if (api.isError(categoryResponse)) {
             notification.error({ message: "Failed to fetch category data", description: categoryResponse.message});
-            this.setState({ error: categoryResponse.message, isLoading: false});
+            this.setState({ ...this.state, isLoading: false});
             return;
         }
+        const categoryInfoResponse = await api.category.getCategoryInfo(categoryResponse.id);
+        if (api.isError(categoryInfoResponse)) {
+            notification.error({message: "Failed to fetch category info", description: categoryInfoResponse.message});
+            this.setState({ ...this.state, isLoading: false});
+            return;
+        }
+        console.log(categoryInfoResponse);
         this.setState({
             activePage: 1,
             products: productResponse,
             category: categoryResponse,
+            categoryInfo: categoryInfoResponse,
             isLoading: false
         });
     }
