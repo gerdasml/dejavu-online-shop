83ab590d2b48f1eda63bb4e1bbd531d988ed126c
Gerda Šimoliūnaitė <sml.gerda@gmail.com>
2018-05-25 12:50:12 +0300

    Added new model

--- djv-order/src/main/java/lt/dejavu/order/model/db/ShippingInformation.java
+++ djv-order/src/main/java/lt/dejavu/order/model/db/ShippingInformation.java
@@ -1,5 +1,6 @@
 package lt.dejavu.order.model.db;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import javax.persistence.*;
 @Setter
 @EqualsAndHashCode
 public class ShippingInformation {
+    @JsonIgnore
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
