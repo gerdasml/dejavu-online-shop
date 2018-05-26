fc89db2cef4edd3544a52c359801dcfdf769cd7c
Mantas <man.petrikas@gmail.com>
2018-05-26 15:56:59 +0300

    resolved n+1 problem, still problem with fetching parent categories

--- djv-product/src/main/java/lt/dejavu/product/service/DiscountService.java
+++ djv-product/src/main/java/lt/dejavu/product/service/DiscountService.java
@@ -2,8 +2,11 @@ package lt.dejavu.product.service;
 
 import lt.dejavu.product.dto.discount.DiscountDto;
 import lt.dejavu.product.dto.discount.ProductDiscountDto;
+import lt.dejavu.product.model.Product;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 public interface DiscountService {
     List<DiscountDto> getAllDiscounts();
@@ -17,4 +20,9 @@ public interface DiscountService {
     void deleteDiscount(long id);
 
     ProductDiscountDto getProductDiscount(long productId);
+
+    ProductDiscountDto getProductDiscount(Product product);
+
+    Map<Long,ProductDiscountDto> getProductsDiscounts(Collection<Product> products);
+
 }
